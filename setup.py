cfa5c3538110b2b3f1a090d777f8590fab73b91f
PinkDev1 <5990@protonmail.com>
2021-01-21 03:30:39 -0300

    Now the strings are replaced before compilation
    lang.json:
      * Spaces -> Tabs
      * Scaped line jumps
    
    setup.py:
      * Bump version
      * Strings get loaded and replaced in-memory, before writing to a file and compiling. afterwards, the TEMP source code is deleted and the result .exe is renamed.

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from distutils.core import setup
 import py2exe
 import os
+import shutil
+import json
+import fileinput
 
-nextVersion="1.0.4"
+nextVersion="3.1.0"
 
 def addCleanDataRecursively(dataDir, folderName):
 	allCleanData = str(addDataRecursively(dataDir, folderName))
@@ -52,6 +55,34 @@ def addDataRecursively(dataDir, folderName):
 	print("\nReturning: " + str(allData) + "\n\n\n\n")
 	return allData
 
+#Make a copy of the original source code
+shutil.copyfile('TerrariaRainbowChat.py', 'TEMP_TerrariaRainbowChat.py')
+
+#Load and parse the contents of the lang.json
+#NOTE: Due to parsing, the line jumps (\n) MUST be scaped, otherwise they'll be interpreted while replacing the strings below.
+lang = json.load(open('lang.json', 'r'))
+
+#English or Spanish?
+targetLanguage = input("Wich language do you want to compile this for? (ENG/ESP): ")
+
+print("\n\nReplacing LangStrings in source code...\n")
+#https://stackoverflow.com/questions/17140886/how-to-search-and-replace-text-in-a-file
+#Open and read source code file as read only
+with open('TEMP_TerrariaRainbowChat.py', 'r') as file:
+	sourceCode = file.read()
+	#file is closed automatically by the 'with'.
+	
+#Replace strings in-memory
+for LANGKey in range(0,len(list(lang.keys()))):
+	#https://stackoverflow.com/questions/3097866/access-an-arbitrary-element-in-a-dictionary-in-python#comment28476565_17085251
+	#https://stackoverflow.com/questions/17140886/how-to-search-and-replace-text-in-a-file
+	sourceCode = sourceCode.replace(str(list(lang.keys())[LANGKey]), str(lang[str(list(lang.keys())[LANGKey])][targetLanguage]) )
+
+#Write changes to the file
+with open('TEMP_TerrariaRainbowChat.py', 'w') as file:
+	file.write(sourceCode)
+	#file is closed automatically by the 'with'.
+
 
 useAHK = input("Do you want to build this with AHK included? (y/n): ")
 
@@ -62,7 +93,7 @@ if(useAHK == "y"):
 
 	print("Good luck.")
 	setup(
-			console=['TerrariaRainbowChat.py'],
+			console=['TEMP_TerrariaRainbowChat.py'],
 			options = {'py2exe': {'bundle_files': 1, 'compressed': True, 'optimize': 2,"dll_excludes": ["libcrypto-1_1.dll", "libssl-1_1.dll"]}},
 			zipfile = None,
 			data_files = ahkTemplates,
@@ -71,9 +102,13 @@ if(useAHK == "y"):
 		)
 else:
 	setup(
-			console=['TerrariaRainbowChat.py'],
+			console=['TEMP_TerrariaRainbowChat.py'],
 			options = {'py2exe': {'bundle_files': 1, 'compressed': True, 'optimize': 2, 'excludes': ['AHK','screeninfo', 'lolpython', 'ahk', 'get_monitors', 'lol_py'], "dll_excludes": ["libcrypto-1_1.dll"]}},
 			zipfile = None,
 			name='Terraria Colourfull Chat',
 			version=nextVersion,
 		)
+
+#Cleanup
+os.remove("TEMP_TerrariaRainbowChat.py")
+os.rename(r'dist/TEMP_TerrariaRainbowChat.exe',r'dist/TerrariaRainbowChat.exe')
