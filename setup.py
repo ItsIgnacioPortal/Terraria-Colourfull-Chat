51db85196b938a8b7ffa36eb9cc30fe9cc2ad335
PinkDev1 <5990@protonmail.com>
2021-02-12 00:22:50 -0300

    Tidy up the compiler
    - Compiled redistributables now have more descriptive names.
    - "nextVersion" replaced with "version" to avoid confusion.
    - Added separator-comments

--- setup.py
+++ setup.py
@@ -4,8 +4,17 @@ import os
 import shutil
 import json
 import fileinput
+import glob
 
-nextVersion="3.1.0"
+version = "4.0.0"
+languages = {
+	"ENG": "ENGLISH",
+	"ESP" : "ESPAÑOL"
+}
+
+#=====================================================================================
+#=====================================================================================
+#=====================================================================================
 
 def addCleanDataRecursively(dataDir, folderName):
 	allCleanData = str(addDataRecursively(dataDir, folderName))
@@ -55,6 +64,10 @@ def addDataRecursively(dataDir, folderName):
 	print("\nReturning: " + str(allData) + "\n\n\n\n")
 	return allData
 
+#=====================================================================================
+#=====================================================================================
+#=====================================================================================
+
 #Make a copy of the original source code
 shutil.copyfile('TerrariaRainbowChat.py', 'TEMP_TerrariaRainbowChat.py')
 
@@ -82,7 +95,7 @@ with open('TEMP_TerrariaRainbowChat.py', 'r') as file:
 		sourceCode = sourceCode.replace( str(list(lang.keys())[LANGKey]), str(lang[str(list(lang.keys())[LANGKey])][targetLanguage]) )
 
 	#Replace version number
-	sourceCode = sourceCode.replace("AUTO-REPLACED-VERSION", nextVersion)
+	sourceCode = sourceCode.replace("AUTO-REPLACED-VERSION", version)
 
 	#Write changes to the file
 	with open('TEMP_TerrariaRainbowChat.py', 'w') as file:
@@ -97,6 +110,7 @@ if useAHK != "Y" and useAHK != "N":
 	useAHK = "N"
 
 
+
 if(useAHK == "Y"):
 	ahkTemplatesDir = str(input("Enter the location of the templates folder in the AHK library (full path): "))
 
@@ -109,7 +123,7 @@ if(useAHK == "Y"):
 			zipfile = None,
 			data_files = ahkTemplates,
 			name ='Terraria Colourfull Chat',
-			version = nextVersion
+			version = version
 		)
 else:
 	setup(
@@ -117,10 +131,15 @@ else:
 			options = {'py2exe': {'bundle_files': 1, 'compressed': True, 'optimize': 2, 'excludes': ['AHK','screeninfo', 'lolpython', 'ahk', 'get_monitors', 'lol_py'], "dll_excludes": ["libcrypto-1_1.dll"]}},
 			zipfile = None,
 			name = 'Terraria Colourfull Chat',
-			version = nextVersion
+			version = version
 		)
 
 #Cleanup
 os.remove('TEMP_TerrariaRainbowChat.py')
-os.remove(r'dist/TerrariaRainbowChat.exe')
-os.rename(r'dist/TEMP_TerrariaRainbowChat.exe',r'dist/TerrariaRainbowChat.exe')
+
+#https://stackoverflow.com/a/1039747/11490425
+for file in glob.glob("dist/TerrariaRainbowChat_" + languages[targetLanguage] + "*"):
+	os.remove(file)
+
+#Give the compiled file a meaningfull name
+os.rename(r'dist/TEMP_TerrariaRainbowChat.exe',"dist/TerrariaRainbowChat_" + languages[targetLanguage] + "_" + version + ".exe")
