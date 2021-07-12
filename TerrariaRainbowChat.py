623d819bf487516a9f3bdd93b2dc12909645aa25
PinkDev1 <5990@protonmail.com>
2021-07-12 00:54:57 -0300

    Added ignore py-lint warning

--- TerrariaRainbowChat.py
+++ TerrariaRainbowChat.py
@@ -56,7 +56,8 @@ print("LANG_CheckingForUpdates")
 #Check for updates
 #Disable plain HTTP warning. We're only getting a version number. Who cares?
 #https://stackoverflow.com/questions/15445981/how-do-i-disable-the-security-certificate-check-in-python-requests
-requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+#https://github.com/PyCQA/pylint/issues/4584
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning) # pylint: disable=no-member
 updateCheck = requests.get('http://api.github.com/repos/PinkDev1/Terraria-Colourfull-Chat/releases/latest', verify=False)
 
 #If something happened, spit an error.
